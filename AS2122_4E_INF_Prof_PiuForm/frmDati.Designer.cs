4c39a2cdaf34818c8e50f1832428f0a211174f75
PC09 <PC09@INFO5-5395>
2022-03-15 10:29:15 +0100

    Aggiungere i file di progetto.

---
+++ AS2122_4E_INF_Prof_PiuForm/frmDati.Designer.cs
@@ -0,0 +1,172 @@
+﻿
+namespace AS2122_4E_INF_Prof_PiuForm
+{
+    partial class frmDati
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtIndirizzo = new System.Windows.Forms.TextBox();
+            this.txtCitta = new System.Windows.Forms.TextBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnAnnulla = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome";
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(138, 10);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(16, 15);
+            this.lblNome.TabIndex = 1;
+            this.lblNome.Text = "...";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Indirizzo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(59, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Città";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(59, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 15);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Telefono";
+            // 
+            // txtIndirizzo
+            // 
+            this.txtIndirizzo.Location = new System.Drawing.Point(138, 50);
+            this.txtIndirizzo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtIndirizzo.Name = "txtIndirizzo";
+            this.txtIndirizzo.Size = new System.Drawing.Size(257, 23);
+            this.txtIndirizzo.TabIndex = 3;
+            // 
+            // txtCitta
+            // 
+            this.txtCitta.Location = new System.Drawing.Point(138, 88);
+            this.txtCitta.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtCitta.Name = "txtCitta";
+            this.txtCitta.Size = new System.Drawing.Size(257, 23);
+            this.txtCitta.TabIndex = 3;
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(138, 125);
+            this.txtTelefono.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(181, 23);
+            this.txtTelefono.TabIndex = 3;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(113, 202);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(136, 39);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Salva";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnAnnulla
+            // 
+            this.btnAnnulla.Location = new System.Drawing.Point(298, 202);
+            this.btnAnnulla.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnAnnulla.Name = "btnAnnulla";
+            this.btnAnnulla.Size = new System.Drawing.Size(136, 39);
+            this.btnAnnulla.TabIndex = 4;
+            this.btnAnnulla.Text = "Annulla";
+            this.btnAnnulla.UseVisualStyleBackColor = true;
+            this.btnAnnulla.Click += new System.EventHandler(this.btnAnnulla_Click);
+            // 
+            // frmDati
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 271);
+            this.Controls.Add(this.btnAnnulla);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.txtCitta);
+            this.Controls.Add(this.txtIndirizzo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "frmDati";
+            this.Text = "Form di dettaglio sui dati";
+            this.Load += new System.EventHandler(this.frmDati_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtIndirizzo;
+        private System.Windows.Forms.TextBox txtCitta;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnAnnulla;
+    }
+}
